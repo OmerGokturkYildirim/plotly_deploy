c769d54481e9dddbe1b88f63bb8636918a9fa41c
OmerGokturkYildirim <61884955+OmerGokturkYildirim@users.noreply.github.com>
2020-05-30 22:32:03 -0700

    Add files via upload

--- plots.js
+++ plots.js
@@ -1,46 +1,6 @@
-function init() {
-
-    var selector = d3.select("#selDataset");
-  
-    d3.json("samples.json").then((data) => {
-
-        var sampleNames = data.names;
-
-        sampleNames.forEach((sample) => {
-            
-            selector
-            
-            .append("option")
-            
-            .text(sample)
-            
-            .property("value", sample);
-
-        });
-        // Use first sample id to build the plots when page is opened
-    var firstID = sampleNames[0];
-    buildMetadata(firstID);
-    buildCharts(firstID);
-    })
-
-    function optionChanged(newSample) {
-
-        buildMetadata(newSample);
-
-        buildCharts(newSample);
-    }
-
-        
-};
-
-    init();
-
 function buildMetadata(sample) {
-
     d3.json("samples.json").then((data) => {
-       
         var metadata = data.metadata;
-
         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
 
         var result = resultArray[0];
@@ -50,116 +10,103 @@ function buildMetadata(sample) {
         PANEL.html("");
 
         Object.entries(result).forEach(([key, value]) => {
-
             PANEL.append("h6").text(key + ': ' + value);
-
         });
     });
 }
+
 function buildCharts(sample) {
-
     d3.json("samples.json").then((data) => {
+        var samplesData = data.samples;
+        var resultArray2 = samplesData.filter(sampleObj => sampleObj.id == sample);
+        var result2 = resultArray2[0]
 
-        var samples = data.samples;
-
-        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-
-        var result = resultArray[0]
-
-        console.log(result);
-
-        var otu_labels = result.otu_labels;
-
-        console.log(otu_labels);
-
-        var id_oto = result.otu_ids;
+        var sampleVals = result2.sample_values;
+        var iDs = result2.otu_ids;
+        var labels = result2.otu_labels;
         
-        console.log(id_oto)
+        var topTenValues = sampleVals.slice(0,10).reverse();
+        var topTenLabels = labels.slice(0,10);
 
-        var sampleValues = result.sample_values;
-                
-        var topValues = sampleValues.slice(0,10).reverse();
-
-        var topLabels = otu_labels.slice(0,10);
-
-        var otu_id = id_oto.slice(0,10).map(otusample => `OTU ${otusample}`).reverse();
+        var OTUlabel = iDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
 
         // barChart for the data
         var bar = {
-            
-            x: topValues,
-            
-            y: otu_id,
-            
-            text: topLabels, 
-           
+            x: topTenValues,
+            y: OTUlabel,
+            text: topTenLabels, 
             type: "bar",
-            
             orientation: "h"
         };
 
         // Data
-        var bar2 = [bar];
+        var barChart = [bar];
 
         // Apply the group bar mode to the layout
-        var layout = {
-           
+        var barChartLayout = {
             title: "Top 10 Bacterial Species",
-            
             margin: {
-               
                 l: 100,
-               
                 r: 100,
-               
                 t: 100,
-               
                 b: 100
             }
         };
 
         // Render the plot to the div tag with id "bar"
-        Plotly.newPlot("bar", bar2, layout);
-         // Building the bubble chart
-         var bubbleChart = {
-            
-            x: id_oto,
-            
-            y: sampleValues,
-            
-            text: otu_labels,
-            
+        Plotly.newPlot("bar", barChart, barChartLayout);
+
+        // Building the bubble chart
+        var bubbleChart = {
+            x: iDs,
+            y: sampleVals,
+            text: labels,
             mode: "markers",
-            
             marker: {
-                
-                size: sampleValues,
-                
-                color: id_oto,
-                
+                size: sampleVals,
+                color: iDs,
                 colorscale: "Jet"
             }
         };
 
-        var bubbleData =[bubbleChart];
+        var bubbleChartData =[bubbleChart];
+
         // Bubble Chart Layout
-        var bubbleLayout = {
-            
+        var bubbleChartLayout = {
             title: "Bacteria Species per Sample",
-            
             showlegend: false,
-           
             xaxis: {
-                
                 title: {
-                    
-                    text: "UTO ID" }
+                    text: "UTO IDs" }
             }
         };
 
         // Render the plot to the div tag with id "bubble"
-        Plotly.plot("bubble", bubbleData, bubbleLayout);
+        Plotly.plot("bubble", bubbleChartData, bubbleChartLayout);
     });
-
-
 }
+
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+            .append("option")
+            .text(sample)
+            .property("value", sample);
+        });
+
+    // Use first sample id to build the plots when page is opened
+    var firstID = sampleNames[0];
+    buildMetadata(firstID);
+    buildCharts(firstID);
+})}
+
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+init();
